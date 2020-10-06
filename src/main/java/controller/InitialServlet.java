32401300816e45cd14d16bbabe319241c645bf18
DenisViskov <denisviskov16@gmail.com>
2020-10-06 12:41:01 +0300

    update

---
+++ src/main/java/controller/InitialServlet.java
@@ -0,0 +1,28 @@
+package controller;
+
+import DAO.*;
+import org.hibernate.SessionFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author Денис Висков
+ * @version 1.0
+ * @since 06.10.2020
+ */
+@WebServlet(name = "InitialServlet", urlPatterns = "/hiden", loadOnStartup = 0)
+public class InitialServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        final SessionFactory sf = HibernateUtil.getSessionFactory();
+        final StoreDAO userDao = new UserDaoImpl(sf);
+        final StoreDAO carDao = new CarDaoImpl(sf);
+        final StoreDAO announcementDao = new AnnouncementDaoImpl(sf);
+        getServletContext().setAttribute("userDao", userDao);
+        getServletContext().setAttribute("carDao", carDao);
+        getServletContext().setAttribute("announcementDao", announcementDao);
+    }
+}
