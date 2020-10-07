992a443b209959908fc0348e245bd83de164a492
DenisViskov <denisviskov16@gmail.com>
2020-10-07 15:34:41 +0300

    update

--- src/main/java/controller/IndexServlet.java
+++ src/main/java/controller/IndexServlet.java
@@ -12,7 +12,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 /**
@@ -25,7 +27,11 @@ public class IndexServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json");
+        PrintWriter writer = resp.getWriter();
+        writer.print(collectJSON());
+        writer.flush();
     }
 
     @Override
@@ -38,9 +44,25 @@ public class IndexServlet extends HttpServlet {
         JSONArray json = new JSONArray();
         List<User> all = userDao.findAll();
         all.forEach(user -> {
-            JSONObject userJson = new JSONObject();
-            Announcement announcement;
+            user.getAnnouncements().forEach(announcement -> {
+                JSONObject announcementJson = new JSONObject();
+                announcementJson.put("Announcement", announcement.getName());
+                announcementJson.put("Car brand", announcement.getCar().getName());
+                announcementJson.put("Car model", announcement.getCar().getModel());
+                announcementJson.put("Car created", announcement.getCar().getCreated());
+                announcementJson.put("Car body type", announcement.getCar().getBodyType());
+                announcementJson.put("Car mileage", announcement.getCar().getMileage());
+                announcementJson.put("description", announcement.getDescription());
+                announcementJson.put("photo", System.getProperty("catalina.base")
+                        + File.separator
+                        + "images"
+                        + File.separator
+                        + announcement.getPhoto());
+                announcementJson.put("status", announcement.isStatus());
+                announcementJson.put("owner", user.getName());
+                json.put(announcementJson);
+            });
         });
-        return null;
+        return json;
     }
 }
